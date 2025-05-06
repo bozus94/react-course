bb2ddc1c8c7fd672f95d7c40f69dd2a5f1b14260
Bryan Ortega <bj_ortega@hotmail.com>
2025-05-06 18:48:22 +0200

    feat: clase-05-useState

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,23 @@ import { Button } from "./components";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const [name, setName] = useState("Hector");
+
 	const moreCount = () => {
+		//batching => agrupa todos los cambios de estado en caso que hubiese mas de uno y renderiza al finalizar los mismos.
 		setCount((count) => count + 1);
+		setCount((count) => count + 1);
+		setCount((count) => count + 1);
+	};
+
+	const changeName = () => {
+		setName("molina");
 	};
 	return (
 		<>
 			<Button label={`count is ${count}`} parentMethod={moreCount} />
+			<p>{name}</p>
+			<Button label={`the name is: ${name}`} parentMethod={changeName} />
 		</>
 	);
 }
