a439c9e7013c76b3cde215196cea95e8b1ac94bb
Bryan Ortega <bj_ortega@hotmail.com>
2025-05-07 17:38:59 +0200

    feat:clase-06-useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
-import { Button } from "./components";
 
 function App() {
-	const [count, setCount] = useState(0);
-	const [name, setName] = useState("Hector");
+	/* explicación useEffect
 
-	const moreCount = () => {
-		//batching => agrupa todos los cambios de estado en caso que hubiese mas de uno y renderiza al finalizar los mismos.
-		setCount((count) => count + 1);
-		setCount((count) => count + 1);
-		setCount((count) => count + 1);
+		//comunicarnos con un endpoint (endpoint => entidad fuera del componente);
+		//operaciones asyncronas
+		//parámetros de entrada
+
+		useEffect(
+			() => {
+				//=> se ejecuta:
+				//=> cuando se monta un componente
+				//=> cuando hay un cambio en el estado que es dependiente
+
+				return () => {
+					//=> el return se ejecuta cuando se destruye el componente
+					//=> se utiliza para ahorrar consumo de memoria
+				};
+			},
+			[] //=> arreglo de dependencia el useEffect esta pendiente de un cambio para ejecuta la lógica dentro de el, si esta vació la lógica se ejecuta cuando monta el componente.
+		); 
+	*/
+
+	const [data, setData] = useState([]);
+	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(false);
+
+	const fetchData = async () => {
+		try {
+			setLoading(true);
+
+			const response = await fetch("https://jsonplaceholder.typicode.com/users");
+			if (!response.ok) throw new Error("Error al cargar los datos");
+
+			const jsonData = await response.json();
+			setData(jsonData);
+		} catch (error) {
+			setError(error as string);
+		} finally {
+			setLoading(false);
+		}
 	};
 
-	const changeName = () => {
-		setName("molina");
-	};
-	return (
-		<>
-			<Button label={`count is ${count}`} parentMethod={moreCount} />
-			<p>{name}</p>
-			<Button label={`the name is: ${name}`} parentMethod={changeName} />
-		</>
-	);
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	if (loading) return <p>cargando .....</p>;
+	if (error) return <p>Ha ocurrido un error: {error.toString()}</p>;
+	return JSON.stringify(data);
 }
 
 export default App;
